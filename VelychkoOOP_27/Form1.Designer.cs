90c8fa9a260a8c740a2a86aee3a385f83b68f1cc
flamingnote-4 <149953547+flamingnote-4@users.noreply.github.com>
2025-04-17 12:01:50 +0000

    27 unfinished

---
+++ VelychkoOOP_27/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace VelychkoOOP_27
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.descLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.titleLabel.Location = new System.Drawing.Point(240, 12);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(129, 14);
+            this.titleLabel.TabIndex = 1;
+            this.titleLabel.Text = "Зараз у директорії:";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(222, 270);
+            this.treeView1.TabIndex = 2;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.treeView1.DoubleClick += new System.EventHandler(this.treeView1_DoubleClick);
+            // 
+            // descLabel
+            // 
+            this.descLabel.AutoSize = true;
+            this.descLabel.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.descLabel.Location = new System.Drawing.Point(240, 38);
+            this.descLabel.Name = "descLabel";
+            this.descLabel.Size = new System.Drawing.Size(113, 14);
+            this.descLabel.TabIndex = 3;
+            this.descLabel.Text = "Зараз у директорії:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 294);
+            this.Controls.Add(this.descLabel);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "Form1";
+            this.Text = "Файлова система - л.р. №27";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label descLabel;
+    }
+}
+
---
+++ VelychkoOOP_27/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VelychkoOOP_27
+{
+    public partial class Form1 : Form
+    {
+        public List<String> checkedDirs = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                if (drive.IsReady)
+                {
+                    treeView1.Nodes.Add(drive.Name);
+                    Console.WriteLine($"Об’єм диску: {drive.TotalSize}");
+                    Console.WriteLine($"Вільний простір: {drive.TotalFreeSpace}");
+                    Console.WriteLine($"Мітка диску: {drive.VolumeLabel}");
+                }
+                Console.WriteLine();
+            }
+        }
+        private void treeView1_DoubleClick(object sender, EventArgs e)
+        {
+            string path, newPath, shortPath;
+            path = treeView1.SelectedNode.FullPath;
+            shortPath = path.Substring(path.LastIndexOf('\\') + 1);
+
+            try
+            {
+                Directory.GetDirectories(path);
+            }
+            catch
+            {
+                if (File.Exists(path)) titleLabel.Text = $"📄 {shortPath}";
+                return;
+            }
+
+            if (!checkedDirs.Contains(path)) checkedDirs.Add(path);
+            else return;
+
+            foreach (string s in Directory.GetDirectories(path))
+            {
+                newPath = Path.Combine(path, s);
+
+                try
+                {
+                    Directory.GetFiles(newPath);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    continue;
+                }
+
+                TreeNode child = treeView1.SelectedNode.Nodes.Add(s.Substring(path.Length).Replace("\\", ""));
+                child.BackColor = Color.LightYellow;
+                child.NodeFont = new Font("Tahoma", 8, FontStyle.Bold);
+            }
+
+
+            foreach (string s in Directory.GetFiles(path))
+            {
+                TreeNode child = treeView1.SelectedNode.Nodes.Add(s.Substring(path.Length).Replace("\\", ""));
+                child.NodeFont = new Font("Tahoma", 8, FontStyle.Regular);
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string path, shortPath;
+            path = treeView1.SelectedNode.FullPath;
+            shortPath = path.Substring(path.LastIndexOf('\\') + 1);
+
+            try
+            {
+                Directory.GetDirectories(path);
+            }
+            catch
+            {
+                if (File.Exists(path))
+                {
+                    FileInfo fileInfo = new FileInfo(path);
+                    
+                    titleLabel.Text = $"📄 {shortPath}";                    
+                    descLabel.Text = $"Час створення: {fileInfo.CreationTime}\n" +
+                        $"Остання зміна: {fileInfo.LastWriteTime}\n" +
+                        $"Розширення: {fileInfo.Extension}";
+                }
+                
+                return;
+            }
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+    
+            titleLabel.Text = $"📂 {shortPath}";
+            descLabel.Text = $"Час створення: {directoryInfo.CreationTime}\n" +
+                $"Остання зміна: {directoryInfo.LastWriteTime}\nКаталог.";
+        }
+    }
+}
